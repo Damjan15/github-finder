af114128e6fb4c6d3d95a26ad0f5e97f530b1d8d
Damjan15 <stamenovdamjan20@gmail.com>
2022-02-27 13:46:37 -0800

    Fetching data in UserResults component, UserItem component created, Spinner component created

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,11 @@
+import Loader from "./assets/spinner.gif"
+
+const Spinner = () => {
+  return (
+    <div className="w-100 mt-20">
+        <img width={180} className="text-center mx-auto" src={Loader} alt="Loading..." />
+    </div>
+  )
+}
+
+export default Spinner
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import UserResults from '../components/users/UserResults'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <UserResults />
+    </>
   )
 }
 

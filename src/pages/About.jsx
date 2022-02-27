41af8365b6b95a6710556da2434a88727ca989f6
Damjan15 <stamenovdamjan20@gmail.com>
2022-02-27 13:18:28 -0800

    Navbar and Footer components created, Home / About / NotFound pages created

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+const About = () => {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+      <p className="mb-4 text-2xl font-light">
+        A React app to search Github profiles and see profile details.
+      </p>
+      <p>Created by: <span className="text-white font-bold">Damjan Stamenov</span></p>
+    </div>
+  )
+}
+
+export default About
